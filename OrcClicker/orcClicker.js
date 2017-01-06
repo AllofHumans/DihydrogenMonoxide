f3bc5f43c8d21ffa7beece43f120ed9eb678a0b3
WitchKing <bealexander.sh@mysummitps.org>
2017-01-06 18:35:44 +0000

    tier 3 of orc clicker completed

--- OrcClicker/orcClicker.js
+++ OrcClicker/orcClicker.js
@@ -38,9 +38,9 @@ function hobbitFarmerAdd() {
         hobbitCostDisplay.innerHTML = hobbitCost;
         hobbitCountDisplay.innerHTML = hobbitCount;
     }
-    if (hobbitCount >= 4) {
+    if (hobbitCount >= 3) {
         rohanSLocked = false;
-        rohanSDisplay = "Rohan Solier (you have " + rohanSCount + "):" + rohanSCost;
+        rohanSDisplay = "Rohan Soldier (you have " + rohanSCount + "):" + rohanSCost;
         rohanSDisplayView.innerHTML = rohanSDisplay;
     }
 
@@ -50,28 +50,65 @@ function autofarm1() {
     scoreDisplay.innerHTML = score;
 }
 
-//Rohirrim Solier
+
+var cheats = document.getElementById("cheat");
+cheats.addEventListener("mousedown", cheating, true);
+function cheating() {
+    score = 10000000000000000000000
+}
+
+//Rohirrim Soldier
 var rohanSCount = 0;
 var rohanSAdd = document.getElementById("buyRohanS");
-var rohanSCost = 300;
+var rohanSCost = 250;
 var rohanSLocked = true;
 var rohanSDisplayView= document.getElementById("RohanSDisplay");
 var rohanSDisplay = "Locked";
 
 rohanSAdd.addEventListener("mousedown", RohanSAdding, true);
-
 function RohanSAdding() {
-    if ((score >= rohanSCost) && (rohanSLocked = false)) {
+    if ((score >= rohanSCost) && (rohanSLocked == false)) {
         rohanSCount += 1;
         autoFarmLevel += 2;
         score -= rohanSCost;
         rohanSCost = rohanSCost * 2;
-        rohanSDisplay = "Rohan Solier (you have " + rohanSCount + "):" + rohanSCost;
+        rohanSDisplay = "Rohan Soldier (you have " + rohanSCount + "):" + rohanSCost;
         rohanSDisplayView.innerHTML = rohanSDisplay;
+     if (rohanSCount >= 2) {
+        gondorSLocked = false;
+        gondorSDisplay = "Gondor Soldier (you have " + gondorSCount + "):" + gondorSCost;
+        gondorSDisplayView.innerHTML = gondorSDisplay;
+    }
+    }
+}
+
+//Gondorian Soldier
+var gondorSCount = 0;
+var gondorSAdd = document.getElementById("buyGondorS");
+var gondorSCost = 750;
+var gondorSLocked = true;
+var gondorSDisplayView= document.getElementById("gondorSDisplay");
+var gondorSDisplay = "Locked";
+
+gondorSAdd.addEventListener("mousedown", gondorSAdding, true);
+
+function gondorSAdding() {
+    if ((score >= gondorSCost) && (gondorSLocked == false)) {
+        gondorSCount += 1;
+        autoFarmLevel += 4;
+        score -= gondorSCost;
+        gondorSCost = gondorSCost * 2;
+        gondorSDisplay = "Gondor Soldier (you have " + gondorSCount + "):" + gondorSCost;
+        gondorSDisplayView.innerHTML = gondorSDisplay;
         
     }
 }
 
+//Elvish Soldier
+//Bilbo Baggins
+//Aragorn
+//Gandoff
+//Eru Ilúvatar (auto-win button)
 
 
 
@@ -98,4 +135,64 @@ function clubAdding() {
         clubCostDisplay.innerHTML = clubCost;
         clubCountDisplay.innerHTML = clubCount;
     }
+    if (clubCount >= 3) {
+        daggerLocked = false;
+        daggerDisplay = "Dagger (you have " + daggerCount + "):" + daggerCost;
+        daggerDisplayView.innerHTML = daggerDisplay;
+    }
 }
+
+//dagger
+var daggerCount = 0;
+var daggerCost = 500;
+var daggerLocked = true;
+var daggerDisplay = "locked";
+var daggerAdd = document.getElementById("buyDagger");
+var daggerDisplayView = document.getElementById("daggerDisplay");
+
+daggerAdd.addEventListener("mousedown", daggerAdding, true);
+
+function daggerAdding() {
+        if ((score >= daggerCost) && (daggerLocked == false)) {
+        daggerCount += 1;
+        clickPower += 2;
+        score -= daggerCost;
+        daggerCost = daggerCost * 2;
+        daggerDisplay = "Dagger (you have " + daggerCount + "):" + daggerCost;
+        daggerDisplayView.innerHTML = daggerDisplay;
+        }
+        if (daggerCount >= 2) {
+        sBowLocked = false;
+        sBowDisplay = "Short Bow (you have " + sBowCount + "):" + sBowCost;
+        sBowDisplayView.innerHTML = sBowDisplay;
+        }
+}
+
+//Shortbow
+
+var sBowCount = 0;
+var sBowCost = 1500;
+var sBowLocked = true;
+var sBowDisplay = "locked";
+var sBowAdd = document.getElementById("buySBow");
+var sBowDisplayView = document.getElementById("sBowDisplay");
+
+sBowAdd.addEventListener("mousedown", sBowAdding, true);
+
+function sBowAdding() {
+        if ((score >= sBowCost) && (sBowLocked == false)) {
+        sBowCount += 1;
+        clickPower += 4;
+        score -= sBowCost;
+        sBowCost = sBowCost * 2;
+        sBowDisplay = "Short Bow (you have " + sBowCount + "):" + sBowCost;
+        sBowDisplayView.innerHTML = sBowDisplay;
+        }
+}
+
+
+//longsowrd
+//battleaxe
+//longbow
+//greatsword
+//wizzard's staff
