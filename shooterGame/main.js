16c5d15dbb8ecdc983b7df80f136233ef3422499
WitchKing <bealexander.sh@mysummitps.org>
2017-12-21 00:42:11 +0000

    removed some stuff and added to shooterGame

--- shooterGame/main.js
+++ shooterGame/main.js
@@ -1,8 +1,8 @@
 // JavaScript File
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-
+const bulletSprite = document.getElementById("bullet");
+const enemySprite = document.getElementById("enemy");
 
 //defining stuff
 const keypresses = {
@@ -13,23 +13,27 @@ const keypresses = {
     rightArrow: false
 };
 const bullets = [];
+const enemies = [];
 const player = {
-    xpos: 50,
+    //☭☭☭
+    hp: 10,
+    xpos: 0,
     ypos: 0,
     spirte: document.getElementById("player"),
     canFire: true,
+    speed: 5,
     move: function() {
         if (keypresses.upArrow) {
-            this.ypos -= 5;
+            this.ypos -= this.speed;
         }
         if (keypresses.downArrow) {
-            this.ypos += 5;
+            this.ypos += this.speed;
         }
         if (keypresses.leftArrow) {
-            this.xpos -= 5;
+            this.xpos -= this.speed;
         }
         if (keypresses.rightArrow) {
-            this.xpos += 5;
+            this.xpos += this.speed;
         }
     },
     boundaries: function() {
@@ -62,19 +66,57 @@ class Bullet {
     constructor() {
         this.xpos = player.xpos;
         this.ypos = player.ypos + 35;
-        this.spirte = document.getElementById("bullet");
     }
     static updateBullets() {
         for (let i = 0; i < bullets.length; i++) {
-            bullets[i].xpos += 10;
-            ctx.drawImage(bullets[i].spirte, bullets[i].xpos, bullets[i].ypos, 7, 7);
+            bullets[i].xpos += 15;
+            ctx.drawImage(bulletSprite, bullets[i].xpos, bullets[i].ypos, 7, 7);
             if (bullets[i].xpos >= 800) {
                 bullets.splice(i, 1);
             }
         }
     }
 }
-
+class Enemy {
+    constructor(x, y) {
+        this.xpos = x;
+        this.ypos = y;
+        this.hp = 3;
+        this.invincible = false;
+    }
+    static updateEnemies() {
+        for (let i = 0; i < enemies.length; i++) {
+            enemies[i].xpos -= 3;
+            ctx.drawImage(enemySprite, enemies[i].xpos, enemies[i].ypos, 75, 50);
+            let deletethis = false;
+            for (let j = 0; j < bullets.length; j++) {
+                //BUG HERE FIX IT!!!!!
+                if ((bullets[j].ypos) > enemies[i].ypos && enemies[i].ypos > (bullets[j].ypos - 50) &&
+                (bullets[j].xpos) > enemies[i].xpos && enemies[i].xpos > (bullets[j].xpos - 75) && 
+                !enemies[i].invincible) {
+                    enemies[i].hp --;
+                    enemies[i].invincible = true;
+                    setTimeout(function() {
+                        enemies[i].invincible = false;    
+                    }, 200);
+                }
+            }
+            if (enemies[i].xpos <= 0 || enemies[i].hp <= 0) {
+            enemies.splice(i, 1);           
+            }
+        }
+    }
+}
+let canSpawnEnemy = true;
+function createEnemy() {
+    if (canSpawnEnemy) {
+        let x = 800;
+        let y = 250 + Math.floor((Math.random() - 0.5) * 400.0);
+        enemies.push(new Enemy(x, y));
+        canSpawnEnemy = false;
+        setTimeout(function() {canSpawnEnemy = true;}, 2000);
+    }
+}
 
 //Event Listeners
 document.addEventListener("keydown", function(e) {
@@ -122,9 +164,12 @@ function gameLoop() {
     player.boundaries();
     player.fire();
     Bullet.updateBullets();
+    Enemy.updateEnemies();
+    createEnemy();
 
 
     window.requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
+createEnemy();
